b50f5ffe64f698cf1b331f97e88c40fb6bde7052
Jordan Sussman <jsussman@workwithopal.com>
2021-10-30 15:14:12 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+## READ ME
+
+**Product Announcement** is an interactive and responsive mock product announcement website using Three.js. By the power of Three.js, as you scroll, the world (product) animates to fly over and away from you. More on Three.js can be found here - https://threejs.org. 
+
+![](/static/img/website.gif)
+
+Built with:
+```
+- Three.js (JavaScript)
+- HTML
+- CSS
+```
+
+To run:
+```
+Install dependencies
+$ npm install
+
+Run server at localhost:8080
+$ npm run dev
+```
---
+++ src/script.js
@@ -0,0 +1,115 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Loading
+const textureLoader = new THREE.TextureLoader()
+const normalTexture = textureLoader.load('/textures/NormalMap.png')
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.SphereBufferGeometry(.5, 64, 64)
+
+// Materials
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.normalMap = normalTexture
+material.color = new THREE.Color(0x292929)
+
+// Mesh
+const sphere = new THREE.Mesh(geometry,material)
+scene.add(sphere)
+
+// Lights
+// Main Light
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+pointLight.intensity = 0.4
+scene.add(pointLight)
+// Red Light
+const pointLight2 = new THREE.PointLight(0xff0000, 2)
+pointLight2.position.set(-1.86,1,-1.65)
+pointLight2.intensity = 3
+scene.add(pointLight2)
+// Blue Light
+const pointLight3 = new THREE.PointLight(0xe1ff, 2)
+pointLight3.position.set(2.13, -3, -1.98)
+pointLight3.intensity = 2
+scene.add(pointLight3)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const updateSphere = (event) => {
+    sphere.position.y = window.scrollY * .001
+    sphere.position.z = window.scrollY * .003
+}
+window.addEventListener('scroll', updateSphere)
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = .5 * elapsedTime
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
